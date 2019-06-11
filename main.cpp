9a50f06bc01d8c444f3248f02f010525d59fefdf
Sunny Aguilar <sunny.aguilar@gmail.com>
2019-06-10 21:37:44 -0700

    bin

--- main.cpp
+++ main.cpp
@@ -1,8 +1,8 @@
 /*********************************************************************
 ** Author:          Sandro Aguilar
 ** Date:            June 10, 2019
-** Description:     Describe
-**
+** Description:     A program that converts decimal integers into
+**                  binary format.
 **
 **
 *********************************************************************/
