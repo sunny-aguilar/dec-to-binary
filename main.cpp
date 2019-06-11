e09cdc47879732c48dce37debbee3ed37b94e98e
Sunny Aguilar <sunny.aguilar@gmail.com>
2019-06-10 21:43:51 -0700

    bin

--- main.cpp
+++ main.cpp
@@ -13,7 +13,15 @@ using std::endl;
 using std::string;
 
 int main() {
-    cout << "Hello, World!" << endl;
+    int number = 0;
+
+    cout << "This program will convert your decimal number " << endl;
+    cout << "into a binary number." << endl;
+    cout << "Enter a number between 1 - 100." << endl;
+    cout << ">> ";
+    cin >> number;
+
+    cout << "You entered the number " << number << "." << endl;
 
     return 0;
 }
