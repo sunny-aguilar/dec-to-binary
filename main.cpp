b403498dbc728c2686584aa22781ae9731770de3
Sunny Aguilar <sunny.aguilar@gmail.com>
2019-06-11 23:20:42 -0700

    bin

--- main.cpp
+++ main.cpp
@@ -10,23 +10,15 @@
 #include <iostream>
 #include <string>
 #include <cmath>
-
 using std::cin;
 using std::cout;
 using std::endl;
 using std::string;
 
-// function prototypes
-string convertDecimal(int num);
 
 int main() {
     Binary bin;
     bin.startProgram();
 
     return 0;
-}
-
-
-string convertDecimal(int num) {
-
 }
