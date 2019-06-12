3e5d7e304146cc69479bd7ff431cbde5281c8a41
Sunny Aguilar <sunny.aguilar@gmail.com>
2019-06-11 22:55:25 -0700

    bin

---
+++ binary.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Sandro Aguilar on 2019-06-11.
+//
+
+#include "binary.hpp"
---
+++ binary.hpp
@@ -0,0 +1,13 @@
+/*********************************************************************
+** Author:          Sandro Aguilar
+** Date:            June 10, 2019
+** Description:     A program that converts decimal integers into
+**                  binary format.
+**
+**
+*********************************************************************/
+
+#ifndef DEC_TO_BINARY_BINARY_HPP
+#define DEC_TO_BINARY_BINARY_HPP
+
+#endif //DEC_TO_BINARY_BINARY_HPP
