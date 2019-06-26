ca3b6184f4f202637328041b997889a4680f9392
Sunny Aguilar <sunny.aguilar@gmail.com>
2019-06-26 13:00:35 -0700

    binary

--- binary.cpp
+++ binary.cpp
@@ -45,7 +45,7 @@ void Binary::startProgram() {
 }
 
 /*********************************************************************
-** Description:     function that converts numbers into its
+** Description:     function that converts numbers into its binary
 *********************************************************************/
 string Binary::convertDecimal(int num) {
     int binNumber = num;
