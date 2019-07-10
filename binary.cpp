e5b5055eb376f5b17156ae8988961c6ca671a41f
Sunny Aguilar <sunny.aguilar@gmail.com>
2019-07-09 21:03:19 -0700

    binary

--- binary.cpp
+++ binary.cpp
@@ -2,7 +2,7 @@
 ** Author:          Sandro Aguilar
 ** Date:            June 10, 2019
 ** Description:     A program that converts decimal integers into
-**                  binary format. Include
+**                  binary format. Includes
 **
 **
 *********************************************************************/
