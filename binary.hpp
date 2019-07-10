73783ca672d8f3a94bf008ffb212a1931505fa46
Sunny Aguilar <sunny.aguilar@gmail.com>
2019-07-09 21:14:17 -0700

    binary

--- binary.hpp
+++ binary.hpp
@@ -3,7 +3,7 @@
 ** Date:            June 10, 2019
 ** Description:     A program that converts decimal integers into
 **                  binary format. Contains spaces in between each
-**
+**                  n
 **
 *********************************************************************/
 #ifndef DEC_TO_BINARY_BINARY_HPP
