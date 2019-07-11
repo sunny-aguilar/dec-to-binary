3c9df3a167f2afc8dd65f1aa0235b9c4ed8a69f8
Sunny Aguilar <sunny.aguilar@gmail.com>
2019-07-10 22:16:51 -0700

    binary

--- binary.cpp
+++ binary.cpp
@@ -60,7 +60,7 @@ string Binary::convertDecimal(int num) {
 
         if (remainder > 0) {
             binString.insert(0, "1");
-            //bin
+            //bi
         }
         else {
             binString.insert(0, "0");
