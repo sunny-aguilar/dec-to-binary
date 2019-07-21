a04b5d124677cf36cc2588eeae0a65f33b233dab
Sunny Aguilar <sunny.aguilar@gmail.com>
2019-07-21 14:10:09 -0700

    binary

--- binary.cpp
+++ binary.cpp
@@ -68,7 +68,7 @@ string Binary::convertDecimal(int num) {
         binNumber = quotient;
     }
 
-    // add zeros to first
+    // add zeros to first n
     if (binString.length() < 4) {
         int addZeros = 4 - binString.length();
         for (int i = 0; i < addZeros; i++) {
